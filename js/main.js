910ae2f0190235491667af445df0e2480d171e75
Alex Bril <brilalex1989@gmail.com>
2016-08-19 00:13:55 +0300

    last mod

--- js/main.js
+++ js/main.js
@@ -1,48 +1,78 @@
-function getID($id){
-    var doc = document;
+function getID( doc, $id ){
+    doc = doc || document;
     if(doc.getElementById($id)) return doc.getElementById($id);
     else return false;
 }
 
 window.onload = function(){
 
-    if( getID('welcome') ){
+    (function(){
+        var doc = document;
+        if( getID(doc, 'welcome') ){
+            var main     =  getID(doc, 'main'),
+                welcome  =  getID(doc, 'welcome'),
+                contact  =  getID(doc, 'contact');
 
-        setTimeout( function(){
-            showElem(getID('main'));
-        }, 500 );
 
-        setTimeout( function(){
-            showElem(getID('welcome'));
-        }, 1000 );
+            setTimeout( function(){
+                animate({
+                    duration: 500,
+                    timing: function(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function(progress) {
+                        main.style.opacity = progress ;
+                    }
+                });
+            }, 500 );
 
-        setTimeout( function(){
-            showElem(getID('contact'));
-            getID('contact').classList.add('transition');
-        }, 1400 );
+            setTimeout( function(){
+                animate({
+                    duration: 1000,
+                    timing: function(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function(progress) {
+                        welcome.style.opacity = progress ;
+                    }
+                });
+            }, 1200 );
 
-        function showElem(elem){
-            var opacity = +elem.style.opacity;
+            setTimeout( function(){
+                animate({
+                    duration: 500,
+                    timing: function(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function(progress) {
+                        contact.style.opacity = progress ;
+                    }
+                });
 
-            elem.style.opacity = 0;
-
-            var int = setInterval(function(){
-                opacity = +(opacity + 0.01).toFixed(2);
-                elem.style.opacity = opacity;
-
-                if( opacity == 1 ){
-                    clearInterval(int)
-                }
-            }, 2);
+                var className = getID(doc, 'contact').className;
+                getID(doc, 'contact').className = className + " transition";
+            }, 1800 );
         }
+    })();
+
+
+    function animate(options) {
+        var start = performance.now();
+
+        requestAnimationFrame(function animates(time) {
+            var timeFraction = (time - start) / options.duration;
+            if (timeFraction > 1) timeFraction = 1;
+            var progress = options.timing(timeFraction);
+
+            options.draw(progress);
+
+            if (timeFraction < 1) {
+                requestAnimationFrame(animates);
+            }
+        });
     }
+
 };
-
-
-
-
-
-
 
 
 
