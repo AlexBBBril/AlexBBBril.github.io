36f8d67bb6576c844e669b596d9864e49d65b357
Alex Bril <brilalex1989@gmail.com>
2016-08-19 23:56:35 +0300

    form connection

--- js/main.js
+++ js/main.js
@@ -7,12 +7,33 @@ function getID( doc, $id ){
 window.onload = function(){
 
     (function(){
-        var doc = document;
-        if( getID(doc, 'welcome') ){
-            var main     =  getID(doc, 'main'),
-                welcome  =  getID(doc, 'welcome'),
-                contact  =  getID(doc, 'contact');
 
+        var doc = document,
+            isExistError = allowSend(),
+            mailMatch = /^[-0-9a-zёа-я_\.]+@[-0-9a-zёа-я_^\.]+\.[a-zёа-я]{2,8}$/i,
+            matchLink = /((https|http)(\:)*(\/)*)*(\S)+(((\.)|(\/)+\S)|(\S)|)*((\.)|(\/))+([a-zа-я]+)(\/*(\S))*((\/)*(\?)*(\S)+(\=|\&)*)*((\.)*(\S](\?|=)*)*())*/i,
+            telephone = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
+
+        function allowSend(){
+            var error = false;
+            return {
+                setError: function(err){
+                    error = err;
+                },
+                getError: function(){
+                    return error;
+                }
+            }
+        }
+
+        if( getID(doc, 'welcome') ){
+            var main       =  getID(doc, 'main'),
+                welcome    =  getID(doc, 'welcome'),
+                contact    =  getID(doc, 'contact'),
+                formMail   =  getID(doc, 'form-mail'),
+                closeForm  =  getID(doc, 'close-form'),
+                formValue  =  doc.getElementsByClassName('form-value');
 
             setTimeout( function(){
                 animate({
@@ -53,6 +74,177 @@ window.onload = function(){
                 getID(doc, 'contact').className = className + " transition";
             }, 1800 );
         }
+
+        contact.onclick = function(){
+            var className = formMail.className,
+                display = /display-none/;
+
+            if( display.test(className)){
+                className = className.replace("display-none", "");
+                formMail.style.opacity = 0;
+                formMail.className = className;
+
+                animate({
+                    duration: 500,
+                    timing: function(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function(progress) {
+                        formMail.style.opacity = progress ;
+                        welcome.style.opacity = -progress ;
+                        contact.style.opacity = -progress ;
+                    }
+                });
+            }
+        };
+
+        main.onclick = function(event){
+            var target      =  event.target,
+                className   =  formMail.className;
+
+            if( target == this && !/display-none/.test(className) ){
+                formMail.className = className + " display-none";
+                animate({
+                    duration: 500,
+                    timing: function(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function(progress) {
+                        welcome.style.opacity = progress ;
+                        contact.style.opacity = progress ;
+                    }
+                });
+            }
+        };
+
+        closeForm.onclick = function(){
+            var className   =  formMail.className;
+
+            if( !/display-none/.test(className) ){
+                formMail.className = className + " display-none";
+                animate({
+                    duration: 500,
+                    timing: function(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function(progress) {
+                        welcome.style.opacity = progress ;
+                        contact.style.opacity = progress ;
+                    }
+                });
+            }
+        };
+
+        formMail.onsubmit = function(){
+            var self        =  this,
+                className   =  this.className;
+
+            for (var i = 0, max = formValue.length; i < max; i++) {
+                var val = formValue[i].value;
+
+                if( val == "" && this.getAttribute("data-type") != "phone" ){
+                    this.value = "Поле должно быть заполнено";
+                    this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    isExistError.setError(true);
+                }
+                if( val != "" && this.getAttribute("data-type") == "name" && matchLink.test(val) ){
+                    this.value = "Извините, ссылки запрещены.";
+                    this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    isExistError.setError(true);
+                }
+                if( val != "" && this.getAttribute("data-type") == "mail" && ! mailMatch.test(val) ){
+                    this.value = "Неправильный ввод e-mail";
+                    this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    isExistError.setError(true);
+                    console.log("mail");
+                }
+                if( val != "" && this.getAttribute("data-type") == "phone" && ! telephone.test(val) ){
+                    this.value = "Неправильный ввод телефона";
+                    this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    isExistError.setError(true);
+                }
+                if( val != "" && this.getAttribute("data-type") == "message" && matchLink.test(val) ){
+                    this.value =  "Извините, ссылки запрещены.";
+                    this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    isExistError.setError(true);
+                }
+
+                if( formValue[i].className.indexOf("red-alert") != -1 ){
+                    isExistError.setError(true);
+                }else{
+                    isExistError.setError(false);
+                }
+
+                if( isExistError.getError() === true ){
+                    return false;
+                }
+                console.log(val);
+            }
+
+
+            setTimeout(function(){
+                animate({
+                    duration: 500,
+                    timing: function(timeFraction) {
+                        return timeFraction;
+                    },
+                    draw: function(progress) {
+                        self.style.opacity    = -progress ;
+                        welcome.style.opacity = progress ;
+                        contact.style.opacity = progress ;
+                    }
+                });
+                self.className = className + " display-none";
+                self.reset();
+            }, 500);
+        };
+
+        (function () {
+            var sendMessage  =  getID(doc, "send-message"),
+                oldValue     =  false;
+
+            for (var i = 0, max = formValue.length; i < max; i++) {
+
+                formValue[i].onblur = function(){
+                    var val = this.value;
+                    if( val == "" && this.getAttribute("data-type") != "phone" ){
+                        this.value = "Поле должно быть заполнено";
+                        this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    }
+                    if( val != "" && this.getAttribute("data-type") == "name" && matchLink.test(val) ){
+                        this.value = "Извините, ссылки запрещены.";
+                        this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    }
+                    if( val != "" && this.getAttribute("data-type") == "mail" && ! mailMatch.test(val) ){
+                        this.value = "Неправильный ввод e-mail";
+                        this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                        console.log("mail");
+                    }
+                    if( val != "" && this.getAttribute("data-type") == "phone" && ! telephone.test(val) ){
+                        this.value = "Неправильный ввод телефона";
+                        this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    }
+                    if( val != "" && this.getAttribute("data-type") == "message" && matchLink.test(val) ){
+                        oldValue   =  val;
+                        this.value =  "Извините, ссылки запрещены.";
+                        this.className = this.className.indexOf("red-alert") == -1 ?  this.className + " red-alert" : this.className;
+                    }
+                };
+                formValue[i].onfocus = function(){
+                    var className = this.className;
+                    if( className.indexOf("red-alert") != -1 ){
+                        className = className.replace("red-alert", "");
+                        this.className = className;
+                        this.value = "";
+                    }
+                    if( oldValue && this.getAttribute("data-type") == "message" ){
+                        this.value = oldValue;
+                        oldValue = false;
+                    }
+                };
+            }
+        })();
+
     })();
 
 
@@ -71,6 +263,8 @@ window.onload = function(){
             }
         });
     }
+
+
 
 };
 
